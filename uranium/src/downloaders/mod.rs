6bfca2f80772ee594a21442da59a7b3751262dae
sergious234 <sergious234@gmail.com>
2023-08-11 20:20:56 +0200

    Add re-exports so its easy to use

--- uranium/src/downloaders/mod.rs
+++ uranium/src/downloaders/mod.rs
@@ -1,6 +1,17 @@
-pub mod curse_downloader;
+pub mod minecraft_downloader;
+
+mod updater;
+mod curse_downloader;
 mod functions;
 mod gen_downloader;
-pub mod minecraft_downloader;
-pub mod rinth_downloader;
-pub mod updater;
+mod rinth_downloader;
+
+// Re-export the structs/functions so the user dont have to:
+// use uranium::downloaders::rinth_downloader::RinthDownloader
+//
+// instead can:
+// use uranium::downloaders::RinthDownloader
+
+pub use self::curse_downloader::curse_modpack_downloader;
+pub use self::rinth_downloader::RinthDownloader;
+pub use self::updater::update_modpack;
