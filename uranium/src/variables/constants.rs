7e9fc12a925fefadcdae169e6fedc8cfc1748813
sergious234 <sergious234@gmail.com>
2023-08-18 20:49:42 +0200

    new constant, launcher_profiles.json

--- uranium/src/variables/constants.rs
+++ uranium/src/variables/constants.rs
@@ -8,6 +8,7 @@ pub const RINTH_JSON: &str = "modrinth.index.json";
 pub const CURSE_JSON: &str = "manifest.json";
 pub const CONFIG_DIR: &str = "config/";
 pub const OVERRIDES_FOLDER: &str = "overrides/";
+pub const PROFILES_FILE: &str = "launcher_profiles.json";
 
 pub static NTHREADS: RwLock<usize> = RwLock::new(32);
 
