211bb43221fef78f3ca9ddcae9f6c8d05db67d35
sergious234 <sergious234@gmail.com>
2023-10-21 13:14:39 +0200

    Clippy pedantic && Docs

--- uranium/src/lib.rs
+++ uranium/src/lib.rs
@@ -1,11 +1,13 @@
 #![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+
 
 use std::path::Path;
 
 use downloaders::{RinthDownloader, minecraft_downloader::MinecraftDownloader};
 use error::{MakerError, UraniumError};
 use log::info;
-use modpack_maker::maker::{ModpackMaker, State};
+use modpack_maker::{ModpackMaker, State};
 use variables::constants::*;
 
 pub mod downloaders;
@@ -22,6 +24,10 @@ mod zipper;
 ///
 /// This function will make a Modpack from the
 /// given path.
+///
+/// # Errors
+/// This function will return a `MakeError` in case the modpack can't
+/// be made for any reason.
 pub async fn make_modpack<I: AsRef<Path>>(minecraft_path: I) -> Result<(), MakerError> {
     let mut maker = ModpackMaker::new(&minecraft_path);
     maker.start()?;
@@ -47,6 +53,11 @@ pub async fn make_modpack<I: AsRef<Path>>(minecraft_path: I) -> Result<(), Maker
 ///
 /// If there is no mods and/or config folder inside `destination_path` then they
 /// will be created.
+///
+///
+/// # Errors
+/// This function will return an `UraniumError` in case the download
+/// fails or when one or more paths are wrong.
 pub async fn rinth_pack_download<I: AsRef<Path>>(
     file_path: I,
     destination_path: I,
@@ -59,6 +70,10 @@ pub async fn rinth_pack_download<I: AsRef<Path>>(
 /// # Easy to go function
 ///
 /// This function still work in progress
+///
+/// # Errors
+/// This function will return an `Err(UraniumError)` in case the `MinecraftDownloader` has an error
+/// during the download.
 pub async fn download_minecraft<I: AsRef<Path>>(
     instance: &str,
     destination_path: I,
@@ -72,6 +87,9 @@ pub async fn download_minecraft<I: AsRef<Path>>(
 ///
 /// Use it carefully, a big number of threads may decrease the performance.
 /// The default number of threads is 32.
+///
+/// # Panics
+/// Will panic in case `RwLockWriteGuard` cant be acquired
 pub fn set_threads(t: usize) {
     let mut aux = NTHREADS.write().unwrap();
     *aux = t;
@@ -82,9 +100,12 @@ pub fn set_threads(t: usize) {
 /// If this function is not called then there will be no
 /// log.txt or any kind of debug info/warn/warning message will
 /// be show in console.
+///
+/// # Panics
+/// Will panic in case log files or `CombinedLogger` cant be created.
 pub fn init_logger() {
     use chrono::prelude::Local;
-    use simplelog::*;
+    use simplelog::{ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode, WriteLogger};
     use std::fs::File;
 
     let log_file_name = format!("log_{}.txt", Local::now().format("%H-%M-%S_%d-%m-%Y"));
