5ede9433bcd84fcc44c1a947f4d6267383b083a0
sergious234 <sergious234@gmail.com>
2025-07-31 13:39:03 +0200

    Change NTHREADS

--- uranium/src/variables/constants.rs
+++ uranium/src/variables/constants.rs
@@ -2,11 +2,13 @@ use std::sync::RwLock;
 
 pub const EXTENSION: &str = "mrpack";
 pub const TEMP_DIR: &str = "./temp_dir/";
-pub const DEFAULT_NTHREADS: usize = 32;
+
+/// In case NTHREADS cant be read this value will be returned
+pub const DEFAULT_NTHREADS: usize = 8;
 pub const RINTH_JSON: &str = "modrinth.index.json";
 pub const CURSE_JSON: &str = "manifest.json";
 pub const CONFIG_DIR: &str = "config/";
 pub const OVERRIDES_FOLDER: &str = "overrides/";
 pub const PROFILES_FILE: &str = "launcher_profiles.json";
 
-pub static NTHREADS: RwLock<usize> = RwLock::new(16);
+pub static NTHREADS: RwLock<usize> = RwLock::new(8);
