da380d65060f44cdba9bef3924581943e082304d
sergious234 <sergious234@gmail.com>
2025-08-03 11:51:53 +0200

    relative routes

--- uranium/tests/installation_verification.rs
+++ uranium/tests/installation_verification.rs
@@ -5,22 +5,24 @@ use uranium_rs::init_logger;
 use uranium_rs::version_checker::InstallationVerifier;
 
 const VERSION: &str = "1.21.7";
+const PATH: &str = "./data/minecraft_test1/";
 #[tokio::test]
 pub async fn test1() {
     let _ = init_logger();
 
-    let mut md = MinecraftDownloader::<Downloader>::init("/home/sergio/.minecraft/", VERSION)
+    let mut md = MinecraftDownloader::<Downloader>::init(PATH, VERSION)
         .await
         .unwrap();
     let res = md.start().await;
 
     if res.is_ok() {
         let x =
-            InstallationVerifier::new(&PathBuf::from("/home/sergio/.minecraft/"), VERSION).await;
+            InstallationVerifier::new(&PathBuf::from(PATH), VERSION).await;
         let _ = x.map(|iv| {
             iv.verify();
         });
     } else {
+        eprintln!("{}", res.err().unwrap());
         panic!("Could not download minecraft")
     }
 }
